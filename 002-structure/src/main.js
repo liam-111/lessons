d7815b9436758e9c28ae7a132e1ee45620dc2ead
liam-111 <tkachyk211@icloud.com>
2024-05-07 17:12:52 +0200

    structure

---
+++ 002-structure/src/main.js
@@ -0,0 +1,7 @@
+console.log('i love money')
+setTimeout(() => {
+    document.querySelector('h1').style.background='orange'  
+}, 3000);
+function change(){
+    document.querySelector('body').style.background='white'  
+ }
