868d5c8a995f9620125964311b2f35da881f43d1
liam-111 <tkachyk211@icloud.com>
2024-05-12 17:27:19 +0200

    count attempts

--- 003-game/src/main.js
+++ 003-game/src/main.js
@@ -1,8 +1,15 @@
-let bollPosition = 2
+let bollPosition = 2;
+let attempts = 10;
+let score = 0;
+
 function cup(n) {
-    console.log("cup", n);
+    attempts--;
+    const attemptsNode = document.querySelector("#attempts")
+    console.log(attemptsNode)
+    attemptsNode.innerHTML = attempts;
+    console.log("cup", n, attempts);
     if (bollPosition == n) alert("cool u find it");
     else alert("No man not your day");
-    bollPosition = Math.round(Math.random())+1;
+    bollPosition = Math.round(Math.random()) + 1;
     console.log(bollPosition);
 }
