702456eef57b84ee4ec4f8e0e2640a3a8170d14c
liam-111 <tkachyk211@icloud.com>
2024-05-17 16:21:23 +0200

    Game done

--- 003-game/src/main.js
+++ 003-game/src/main.js
@@ -12,13 +12,23 @@ function cup(n) {
         score++
         const scoreNode = document.querySelector("#score")
         console.log(attemptsNode)
-        scoreNode.innerHTML = score;    
+        scoreNode.innerHTML = score;
         alert("cool u find it")
-     } else alert("No man not your day");
+    } else alert("No man not your day");
     bollPosition = Math.round(Math.random()) + 1;
     console.log(bollPosition);
-    if (attempts==0){
+    if (attempts == 0) {
         alert("Game over")
         location.reload()
     }
+    const cupNode1 = document.querySelector(".cup-1")
+    const cupNode2 = document.querySelector(".cup-2")
+    cupNode1.classList.add("animation-1")
+    cupNode2.classList.add("animation-2")
+    setTimeout(function () {
+        cupNode1.classList.remove("animation-1")
+        cupNode2.classList.remove("animation-2")
+    }, 3600)
+
+
 }
