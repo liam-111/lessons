16d646a1fca9cbdaab857ee0ef4147b0665a18e2
liam-111 <tkachyk211@icloud.com>
2024-05-22 17:40:01 +0200

    new details

--- 006-data-types/data-types.js
+++ 006-data-types/data-types.js
@@ -1,16 +1,37 @@
 // number
 let x = 1
+
 // string
 let y = "hallo world"
+
 // boolean
 let b = true
 let c = false
-if(b!=c)console.log("b!=c")
+if (b != c) console.log("b!=c")
 if (2 == 3) console.log("if")
 console.log(2 == 2, 2 == 3, 5 > 3, x == 1)
 let username = "Liam"
 if (username == "Liam-1") console.log("yes")
-    else console.log("no")
+else console.log("no")
+
 // undefined 
 let u
 console.log(u)
+
+// Array
+let arr = [1, 2, 3, "boy", true, false, undefined]
+console.log(arr)
+
+// object
+let o = {
+    name: "Tim",
+    age: 28,
+    gender: "man",
+    isStudent: true,
+    say: function () {
+        console.log("Hi")
+    }
+}
+console.log(o)
+o.say()
+console.error("sss")
